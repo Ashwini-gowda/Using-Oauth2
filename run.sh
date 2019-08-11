17c8749ef7752414bfbac5819874dd526ea8dae7
Shivani <shivani.raj@nighthack.in>
2019-08-11 21:04:59 +0530

    using oauth2

---
+++ run.sh
@@ -0,0 +1,10 @@
+export FN_AUTH_REDIRECT_URI = "http://localhost:5000/google/auth"
+export FN_BASE_URI = "http://localhost:5000"
+export FN_CLIENT_ID = "1064463263580-8lm1ca4hlas4d4lmks7u5s8b87c7tqol.apps.googleusercontent.com"
+export FN_CLIENT_SECRET = "x6nd80hkY4Nfka9YzFY72KiX"
+
+export FLASK_APP = app.py
+export FLASK_DEBUG = 1
+export FN_FLASK_SECRET_KEY = SOMETHING RANDOM AND SECRET
+
+
